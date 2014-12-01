5d5f5ae1fe2b00454604637974584fe8458f33fa
dreamsunday@163.com <dreamsunday@163.com>
2014-12-01 18:20:18 +0800

    version1.1.1

--- src/test/java/com/somnus/excel/ExcelTest.java
+++ src/test/java/com/somnus/excel/ExcelTest.java
@@ -26,6 +26,21 @@ public class ExcelTest extends AbstractTestSupport {
 		}
 	}
 	@Test
+	public void test11(){
+		CommonTemplate temp = (CommonTemplate)ApplicationContextHolder.getBean(CommonTemplate.class);
+		try {
+			OutputStream out2 = new FileOutputStream("E://d.xlsx");
+			
+			Workbook workbook =temp.exportReport(new String[] { "1日", "2日","3日", "4日", "5日", "6日", "7日", "8日", "9日", "10日",
+					"11日", "12日","13日", "14日", "15日", "16日", "17日", "18日", "19日", "20日",
+					"21日", "22日","23日", "24日", "25日", "26日", "27日", "28日", "29日", "30日"});
+			workbook.write(out2);
+			out2.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Test
 	public void test2(){
 		CommonVelocity temp = (CommonVelocity)ApplicationContextHolder.getBean(CommonVelocity.class);
 		try {
