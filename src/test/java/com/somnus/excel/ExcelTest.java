e7ab4a1c47d94e84b4754b88a863596a801c5d18
dreamsunday@163.com <dreamsunday@163.com>
2014-12-09 10:37:32 +0800

    version1.1.1

--- src/test/java/com/somnus/excel/ExcelTest.java
+++ src/test/java/com/somnus/excel/ExcelTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import com.somnus.AbstractTestSupport;
 import com.somnus.ApplicationContextHolder;
+import com.somnus.support.util.excel.JxlsUtils;
 import com.somnus.support.util.excel.impl.CommonTemplate;
 import com.somnus.support.util.velocity.CommonVelocity;
 
@@ -34,6 +35,7 @@ public class ExcelTest extends AbstractTestSupport {
 			Workbook workbook =temp.exportReport(new String[] { "1日", "2日","3日", "4日", "5日", "6日", "7日", "8日", "9日", "10日",
 					"11日", "12日","13日", "14日", "15日", "16日", "17日", "18日", "19日", "20日",
 					"21日", "22日","23日", "24日", "25日", "26日", "27日", "28日", "29日", "30日"});
+			JxlsUtils.cleanRepeatedMergedRegion(workbook);
 			workbook.write(out2);
 			out2.close();
 		} catch (Exception e) {
