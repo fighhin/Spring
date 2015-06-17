b7d6380af199ae0b6138f464605a279336a7eb49
love-somnus <928200207@qq.com>
2015-06-17 17:32:21 +0800

    version1.1.1

--- src/main/java/com/somnus/annotation/aop/GreetAspector.java
+++ src/main/java/com/somnus/annotation/aop/GreetAspector.java
@@ -25,7 +25,7 @@ import org.springframework.stereotype.Component;
 @Aspect  
 @Component 
 public class GreetAspector {
-    /**
+    /**@前置通知
      * 方法开始之前执行一段代码
      * @param joinPoint
      */
@@ -36,7 +36,7 @@ public class GreetAspector {
         System.out.println("The method 【" + methodName + "】 begins with " + Arrays.asList(args));
     }
     
-    /**
+    /**@后置最终通知
      * 方法执行之后执行一段代码
      * 无论该方法是否出现异常
      * @param joinPoint
@@ -48,7 +48,7 @@ public class GreetAspector {
         System.out.println("The method 【" + methodName + "】 ends with " + Arrays.asList(args));
     }
     
-    /**
+    /**@后置返回通知
      * 方法正常结束后执行的代码，不包括抛出异常的情况
      * 返回通知是可以访问到方法的返回值的
      * @param joinPoint
@@ -60,7 +60,7 @@ public class GreetAspector {
         System.out.println("The method 【" + methodName + "】 return with " + result);
     }
     
-    /**
+    /**@后置异常通知
      * 在方法出现异常时会执行的代码
      * 可以访问到异常对象，可以指定在出现特定异常时在执行通知代码
      * @param joinPoint
@@ -72,7 +72,7 @@ public class GreetAspector {
         System.out.println("The method " + methodName + " occurs exception: " + ex);
     }
     
-    /**
+    /**@环绕通知
      * 环绕通知需要携带ProceedingJoinPoint类型的参数
      * 环绕通知类似于动态代理的全过程：ProceedingJoinPoint类型的参数可以决定是否执行目标方法。
      * 而且环绕通知必须有返回值，返回值即为目标方法的返回值
@@ -90,15 +90,15 @@ public class GreetAspector {
             //前置通知
             System.out.println("ARROUND-->The method 【" + methodName + "】 begins with " + Arrays.asList(pjp.getArgs()));
             result = pjp.proceed();
-            //返回通知
-            System.out.println("ARROUND-->The method 【" + methodName + "】 return with " + result);
+            //后置最终通知【方法执行之后执行一段代码，无论该方法是否出现异常】
+            System.out.println("ARROUND-->The method 【" + methodName + "】 ends with " + Arrays.asList(pjp.getArgs()));
         } catch (Throwable e) {
-            //异常通知
+            //后置异常通知【在方法出现异常时会执行的代码】
             System.out.println("ARROUND-->The method 【" + methodName + "】 occurs expection : " + e);
             throw new RuntimeException(e);
         }
-        //后置通知
-        System.out.println("ARROUND-->The method 【" + methodName + "】 ends with " + Arrays.asList(pjp.getArgs()));
+        //后置返回通知【方法正常结束后执行的代码，不包括抛出异常的情况】
+        System.out.println("ARROUND-->The method 【" + methodName + "】 return with " + result);
         return result;
     }
     
