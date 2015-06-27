27d012fc6618f623d622a4d692bb72a1b6cc4f1f
love-somnus <928200207@qq.com>
2015-06-27 22:19:58 +0800

    version1.1.1

---
+++ src/main/java/com/somnus/factory/Somnus.java
@@ -0,0 +1,18 @@
+package com.somnus.factory;
+
+import org.springframework.stereotype.Component;
+
+/** 
+ * @Title: Somnus.java 
+ * @Package com.somnus.factory 
+ * @Description: TODO
+ * @author Somnus
+ * @date 2015年6月27日 下午9:44:27 
+ * @version V1.0 
+ */
+@Component
+public class Somnus{
+	public void say(){
+		System.out.println("day day up,study hard hard");
+	}
+}
