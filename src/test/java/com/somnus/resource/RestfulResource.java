66a3eb4dff9eb2c631b6e83379329478bdd298a0
love-somnus <928200207@qq.com>
2016-04-14 22:39:18 +0800

    v

--- src/test/java/com/somnus/resource/RestfulResource.java
+++
@@ -1,9 +0,0 @@
-package com.somnus.resource;
-
-import com.somnus.domain.Request;
-import com.somnus.domain.Response;
-
-public interface RestfulResource {
-	
-	public Response getAccount(Request request);
-}
