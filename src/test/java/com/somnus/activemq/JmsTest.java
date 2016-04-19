ad60552f81ccbfdc35e47c7863eafd175e8ba7ca
love-somnus <928200207@qq.com>
2016-04-19 23:42:13 +0800

    version1.1.1

--- src/test/java/com/somnus/activemq/JmsTest.java
+++ src/test/java/com/somnus/activemq/JmsTest.java
@@ -12,11 +12,25 @@ public class JmsTest extends AbstractTestSupport {
     public void testSend() {
         JmsService jms = (JmsService) ApplicationContextHolder.getBean(JmsServiceImpl.class);  
         jms.sendMessage("你好，生产者！这是消息");
+        try {
+			//防止Spring容器过早的关闭
+			Thread.sleep(Integer.MAX_VALUE);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     @Test
     public void testSend2() {
         JmsService jms = (JmsService) ApplicationContextHolder.getBean(JmsServiceImpl.class);  
         jms.sendMessage(new Message("你学会了ActiveMQ吗","It's too diffcult to study ActiveMQ"));
+        try {
+			//防止Spring容器过早的关闭
+			Thread.sleep(Integer.MAX_VALUE);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
