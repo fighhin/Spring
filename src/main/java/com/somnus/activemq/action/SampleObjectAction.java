ec3913fffaafbaf87951913be75f59bb82dd3e0a
somnus <928200207@qq.com>
2016-11-30 20:12:30 +0800

    ActiveMQ进一步优化

--- src/main/java/com/somnus/activemq/action/SampleObjectAction.java
+++ src/main/java/com/somnus/activemq/action/SampleObjectAction.java
@@ -1,22 +1,19 @@
 package com.somnus.activemq.action;
 
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.somnus.activemq.support.listener.AbstractJmsReceiveListener;
+import com.somnus.activemq.support.listener.AbstractJmsReceiveListener2;
 
 @Component
-public class SampleObjectAction extends AbstractJmsReceiveListener{
+public class SampleObjectAction extends AbstractJmsReceiveListener2{
 	
 	private transient Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Override
-	protected void execute(Message message) throws Exception {
-		log.info("接收序列化的 Java对象消息：{}", ((ObjectMessage)message).getObject());
+	protected void execute(Object message) throws Exception {
+		log.info("接收序列化的 Java对象消息：{}", message);
 		//TODO
 		//消费者拿到想要的Obejct,至于怎么处理就是你自己的事情了
 	}
