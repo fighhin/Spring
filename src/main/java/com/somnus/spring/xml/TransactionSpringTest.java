83527aca792e43f9255be1bea712f046954b08e4
love-somnus <928200207@qq.com>
2017-04-08 19:05:41 +0800

    事务传播

--- src/main/java/com/somnus/spring/xml/TransactionSpringTest.java
+++ src/main/java/com/somnus/spring/xml/TransactionSpringTest.java
@@ -6,70 +6,80 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.somnus.ApplicationContextHolder;
-import com.somnus.spring.xml.transaction.UserService;
-import com.somnus.spring.xml.transaction.UserServiceNew;
+import com.somnus.spring.xml.transaction.service.UserService01;
+import com.somnus.spring.xml.transaction.service.UserService02;
+import com.somnus.spring.xml.transaction.service.UserService03;
+import com.somnus.spring.xml.transaction.service.UserService04;
+import com.somnus.spring.xml.transaction.service.UserService11;
+import com.somnus.spring.xml.transaction.service.UserService11_;
+import com.somnus.spring.xml.transaction.service.UserService12;
+import com.somnus.spring.xml.transaction.service.UserService13;
+import com.somnus.spring.xml.transaction.service.UserService14;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:spring-transaction.xml")
+@ContextConfiguration(locations = {
+		"classpath:spring-transaction.xml",
+		"classpath:spring-mybatis.xml"})
 public class TransactionSpringTest {
+	
 	@Test
-	public void test1(){
-		UserService service = ApplicationContextHolder.getBean(UserService.class);
+	public void test01(){
+		UserService01 service = ApplicationContextHolder.getBean(UserService01.class);
 		System.out.println(service.getClass());
-		service.A1();
+		service.A();
 	}
 	@Test
-	public void test2(){
-		UserService service = ApplicationContextHolder.getBean(UserService.class);
+	public void test02(){
+		UserService02 service = ApplicationContextHolder.getBean(UserService02.class);
 		System.out.println(service.getClass());
-		service.A11();
+		service.A();
 	}
 	
 	@Test
-	public void test3(){
-		UserService service = ApplicationContextHolder.getBean(UserService.class);
+	public void test03(){
+		UserService03 service = ApplicationContextHolder.getBean(UserService03.class);
 		System.out.println(service.getClass());
-		service.A2();
+		service.A();
 	}
 	@Test
-	public void test4(){
-		UserService service = ApplicationContextHolder.getBean(UserService.class);
+	public void test04(){
+		UserService04 service = ApplicationContextHolder.getBean(UserService04.class);
 		System.out.println(service.getClass());
-		service.A22();
+		service.A();
 	}
 	
 	///////////////////////////////////////////////////////////////////
 	
 	@Test
-	public void case1(){
-		UserServiceNew service = ApplicationContextHolder.getBean(UserServiceNew.class);
+	public void test11(){
+		UserService11 service = ApplicationContextHolder.getBean(UserService11.class);
 		System.out.println(service.getClass());
-		service.A1();
+		service.A();
 	}
 	@Test
-	public void case1_(){
-		UserServiceNew service = ApplicationContextHolder.getBean(UserServiceNew.class);
+	public void test11_(){
+		UserService11_ service = ApplicationContextHolder.getBean(UserService11_.class);
 		System.out.println(service.getClass());
-		service.A1_();
+		service.A();
 	}
 	@Test
-	public void case2(){
-		UserServiceNew service = ApplicationContextHolder.getBean(UserServiceNew.class);
+	public void test12(){
+		UserService12 service = ApplicationContextHolder.getBean(UserService12.class);
 		System.out.println(service.getClass());
-		service.A11();
+		service.A();
 	}
 	
 	@Test
-	public void case3(){
-		UserServiceNew service = ApplicationContextHolder.getBean(UserServiceNew.class);
+	public void test13(){
+		UserService13 service = ApplicationContextHolder.getBean(UserService13.class);
 		System.out.println(service.getClass());
-		service.A2();
+		service.A();
 	}
 	@Test
-	public void case4(){
-		UserServiceNew service = ApplicationContextHolder.getBean(UserServiceNew.class);
+	public void test14(){
+		UserService14 service = ApplicationContextHolder.getBean(UserService14.class);
 		System.out.println(service.getClass());
-		service.A22();
+		service.A();
 	}
 	
 }
