e9ea2b1b31ee1f4a1ae79ebb47426352ab7bafd4
love-somnus <928200207@qq.com>
2017-07-06 16:39:46 +0800

    love-somnus <928200207@qq.com>

--- src/main/java/com/somnus/spring/xml/aop/ApologyImpl.java
+++
@@ -1,9 +0,0 @@
-package com.somnus.spring.xml.aop;
-
-public class ApologyImpl implements Apology {  
-   
-    @Override  
-    public void saySorry(String name) {  
-        System.out.println("Sorry! " + name);  
-    }  
-}  
