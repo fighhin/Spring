c1a6392e807f7bc7600847659371695e50b3eac1
love-somnus <928200207@qq.com>
2017-07-31 14:35:47 +0800

    love-somnus <928200207@qq.com>

--- src/main/java/com/somnus/spring/xml/factory/BeanFactory.java
+++ src/main/java/com/somnus/spring/xml/factory/BeanFactory.java
@@ -10,8 +10,8 @@ package com.somnus.spring.xml.factory;
  */
 public class BeanFactory {
 	
-	public static User getInstance(Somnus somnus){
-		System.out.println(somnus.say());
+	public static User getInstance(Somnus somnus, String name){
+		System.out.println(name + "---->" +somnus.say());
 		return new User();
 	}
 	
